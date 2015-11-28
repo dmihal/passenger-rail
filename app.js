373eb534ffd5c8b340d7c627d6404b358d856dfe
David Mihal <eastsideski@gmail.com>
2015-11-28 15:30:05 -0500

    Display all maps

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+kml_layers = [
+  'mbta-cr',
+  'bart',
+  'path',
+  'septa'
+];
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 11,
     center: {lat: 41.876, lng: -87.624}
   });
 
-  var kml = new google.maps.KmlLayer({
-    url: location.href + 'kml/mbta-cr.kml',
-    map: map
+  var layers = kml_layers.map(function(name){
+    return new google.maps.KmlLayer({
+      url: location.href + 'kml/' + name + '.kml',
+      map: map
+    });
   });
 }
